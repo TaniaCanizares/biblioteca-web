18cbd2244936e44dfb4902ba3fe2fb24bbd06888
estivenp <estivenp@unicauca.edu.co>
2019-06-03 11:40:25 -0500

    Ajustes javaScript detalle

--- js/main2.js
+++ js/main2.js
@@ -1,43 +1,55 @@
 (function ($) {
-  "use strict";
-	
-	var lib=2;
-	var libros;
-  // Preloader
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
+	"use strict";
+
+	var lib = 0;
+	// Preloader
+	$(window).on('load', function () {
+		if ($('#preloader').length) {
+			$('#preloader').delay(100).fadeOut('slow', function () {
+				$(this).remove();
+			});
 		}
-		var libros =llenarBib();
-		detalles(libros[lib]);
+		var nombre = obtenerValorUrl();
+		var libros = llenarBib();
+		var pos = buscarLibro(libros,nombre);
+		alert(pos);
+		detalles(libros[pos]);
 	});
 
-	/*function ir_a_detalle(nombre) {
-		pagina ="detalle-libro.html?"
-	  }*/
-
-	function libroDetalle(numLibro){
-		alert(numLibro);
-		console.log(numLibro);
+	function buscarLibro(libros,nomLibro){
+		for(var i=0;i<libros.length;i++){
+			if(libros[i].titilo==nomLibro){
+				console.log("entro");
+				return i;
+			}
+		}
+		return -1;
 	}
-	
-	var llenarBib=function(){
-		var libro1 = new libro("Calculo Diferencia e Integral","Paulo Boulos y Zara Issa Abud","Cientifico",
-		"2","3","1","./img/calculo.jpg","El cálculo diferencial es una parte del análisis matemático que consiste en el estudio de cómo cambian las funciones cuando sus variables cambian. El principal objeto de estudio en el cálculo diferencial es la derivada. Una noción estrechamente relacionada es la de diferencial de una función.");
 
-		var libro2 = new libro("La Rebelion de las ratas","Fernando Soto Aparicio","Ficcion","1","3","2",
-		"./img/la_rebelion_de_las_ratas.jpg","La Rebelión de las Ratas es una obra del escritor colombiano Fernando Soto Aparicio. Cuenta la historia de Timbalí, un pueblo ficticio que el escritor ubica en el departamento de Boyacá y que se encuentra bajo el dominio de las multinacionales estadounidenses");
+	function obtenerValorUrl(numLibro) {
+		//alert(window.location.search.substring(1));
+		var variables = window.location.search.substring(1);
+		var varNombre = variables.split("=");
+		var libro=varNombre[1];
+		//alert("El nombre de la variable que llega es: "+libro);
+		return libro;
+	}
 
-		var libro3 = new libro("It","Stephen King","Terror","1","3","0",
-		"./img/it.jpg","La historia se desarrolla alternada en dos épocas: el pasado (1957-58) y el presente (1985). Los hechos acontecen en la comunidad de Derry, en el estado de Maine, Estados Unidos. Bajo la ciudad, a un nivel inconsciente para todos los pobladores, habita un monstruo despiadado de apetito insaciable; esta malévola criatura atormenta a la comunidad desde tiempos remotos. El monstruo es un ser ajeno a este mundo, y se manifiesta como sus miedos, por lo cual constituye su alimento (preferiblemente los de los niños y adolescentes, cuyos temores son muy sencillos de elaborar). Su disfraz más común es el de un payaso, Pennywise, con el que atrae a sus víctimas.");
+	var llenarBib = function () {
+		var libro1 = new libro("Calculo Diferencia e Integral", "Paulo Boulos y Zara Issa Abud", "Matematicas",
+			"2", "3", "1", "./img/calculo.jpg", "El cálculo diferencial es una parte del análisis matemático que consiste en el estudio de cómo cambian las funciones cuando sus variables cambian. El principal objeto de estudio en el cálculo diferencial es la derivada. Una noción estrechamente relacionada es la de diferencial de una función.");
 
-		var libros = [libro1,libro2,libro3];
+		var libro2 = new libro("La Rebelion de las ratas", "Fernando Soto Aparicio", "Ficcion", "1", "3", "2",
+			"./img/la_rebelion_de_las_ratas.jpg", "La Rebelión de las Ratas es una obra del escritor colombiano Fernando Soto Aparicio. Cuenta la historia de Timbalí, un pueblo ficticio que el escritor ubica en el departamento de Boyacá y que se encuentra bajo el dominio de las multinacionales estadounidenses");
+
+		var libro3 = new libro("It", "Stephen King", "Terror", "1", "3", "0",
+			"./img/it.jpg", "La historia se desarrolla alternada en dos épocas: el pasado (1957-58) y el presente (1985). Los hechos acontecen en la comunidad de Derry, en el estado de Maine, Estados Unidos. Bajo la ciudad, a un nivel inconsciente para todos los pobladores, habita un monstruo despiadado de apetito insaciable; esta malévola criatura atormenta a la comunidad desde tiempos remotos. El monstruo es un ser ajeno a este mundo, y se manifiesta como sus miedos, por lo cual constituye su alimento (preferiblemente los de los niños y adolescentes, cuyos temores son muy sencillos de elaborar). Su disfraz más común es el de un payaso, Pennywise, con el que atrae a sus víctimas.");
+
+		var libros = [libro1, libro2, libro3];
 		return libros;
 	}
 
-	function libro(titilo,autor,genero,edicion,uni_existente,uni_disponible,imagen,descipcion){
+	function libro(titilo, autor, genero, edicion, uni_existente, uni_disponible, imagen, descipcion) {
 		this.titulo = titilo,
 		this.autor = autor,
 		this.genero = genero,
@@ -76,28 +88,28 @@
 
 
 
-  // Back to top button
- /* $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
-  */
+	// Back to top button
+	/* $(window).scroll(function() {
+	   if ($(this).scrollTop() > 100) {
+		 $('.back-to-top').fadeIn('slow');
+	   } else {
+		 $('.back-to-top').fadeOut('slow');
+	   }
+	 });
+	 $('.back-to-top').click(function(){
+	   $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+	   return false;
+	 });
+	 */
 	var nav = $('nav');
 	var navHeight = nav.outerHeight();
 
 	/*--/ ScrollReveal /Easy scroll animations for web and mobile browsers /--*/
-	window.sr = ScrollReveal();
+	/*window.sr = ScrollReveal();
 	sr.reveal('.foo', { duration: 1000, delay: 15 });
 
 	/*--/ Carousel owl /--*/
-	$('#carousel').owlCarousel({
+	/*$('#carousel').owlCarousel({
 		loop: true,
 		margin: -1,
 		items: 1,
@@ -109,7 +121,7 @@
 	});
 
 	/*--/ Animate Carousel /--*/
-	$('.intro-carousel').on('translate.owl.carousel', function () {
+	/*$('.intro-carousel').on('translate.owl.carousel', function () {
 		$('.intro-content .intro-title').removeClass('zoomIn animated').hide();
 		$('.intro-content .intro-price').removeClass('fadeInUp animated').hide();
 		$('.intro-content .intro-title-top, .intro-content .spacial').removeClass('fadeIn animated').hide();
@@ -150,7 +162,7 @@
 	});
 
 	/*--/ Property owl /--*/
-	$('#property-carousel').owlCarousel({
+	/*$('#property-carousel').owlCarousel({
 		loop: true,
 		margin: 30,
 		responsive: {
@@ -167,9 +179,9 @@
 	});
 
 	/*--/ Property owl owl /--*/
-	$('#property-single-carousel').owlCarousel({
+	/*$('#property-single-carousel').owlCarousel({
 		loop: true,
-		margin: 0,  
+		margin: 0,
 		nav: true,
 		navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
 		responsive: {
@@ -180,11 +192,11 @@
 	});
 
 	/*--/ News owl /--*/
-	$('#new-carousel').owlCarousel({
+	/*$('#new-carousel').owlCarousel({
 		loop: true,
 		margin: 30,
 		responsive: {
-			0: {  
+			0: {
 				items: 1,
 			},
 			769: {
@@ -197,7 +209,7 @@
 	});
 
 	/*--/ Testimonials owl /--*/
-	$('#testimonial-carousel').owlCarousel({
+	/*$('#testimonial-carousel').owlCarousel({
 		margin: 0,
 		autoplay: true,
 		nav: true,
@@ -211,6 +223,6 @@
 				items: 1,
 			}
 		}
-	});
+	});*/
 
 })(jQuery);
